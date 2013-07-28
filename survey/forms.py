07cf1b4d4492f5720daf077b587ac9fba028c5db
rhidev.max.hutman.net <rhidev@max.hutman.net>
2013-07-27 20:33:41 -0500

    Initial commit.

---
+++ survey/forms.py
@@ -0,0 +1,93 @@
+from django import forms
+from django_localflavor_us.forms import USZipCodeField
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Fieldset, ButtonHolder
+from crispy_forms.bootstrap import InlineRadios
+
+from survey.models import *
+
+PAGE_THREE_CHOICE_SET = ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7))
+
+class PageOne(forms.ModelForm):
+    zipcode = USZipCodeField()
+
+    def __init__(self, *args, **kwargs):
+        super(PageOne, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.add_input(Submit('submit', 'Next Page'))
+
+    class Meta:
+        model = Survey
+        fields = ('age', 'state', 'zipcode', 'sexual_orientation', 'sexual_orientation_other', 'gender_identity', 'gender_identity_other', 'gender_assigned',
+                    'ethnicity', 'ethnicity_other',)
+
+class PageTwo(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PageTwo, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.add_input(Submit('submit', 'Next Page'))
+
+    class Meta:
+        model = Survey
+        fields = ('current_body_shape_shoulders', 'current_body_shape_waist', 'current_body_shape_hips',
+                    'ideal_body_shape_shoulders', 'ideal_body_shape_waist', 'ideal_body_shape_hips',)
+
+class PageThree(forms.ModelForm):
+    exercise_stay_health = forms.ChoiceField(widget = forms.RadioSelect(), choices=PAGE_THREE_CHOICE_SET)
+    exercise_lose_weight = forms.ChoiceField(widget = forms.RadioSelect(), choices=PAGE_THREE_CHOICE_SET)
+    exercise_improve_appearance = forms.ChoiceField(widget = forms.RadioSelect(), choices=PAGE_THREE_CHOICE_SET)
+    exercise_energize = forms.ChoiceField(widget = forms.RadioSelect(), choices=PAGE_THREE_CHOICE_SET)
+    exercise_sport = forms.ChoiceField(widget = forms.RadioSelect(), choices=PAGE_THREE_CHOICE_SET)
+    exercise_fun = forms.ChoiceField(widget = forms.RadioSelect(), choices=PAGE_THREE_CHOICE_SET)
+    exercise_muscle  = forms.ChoiceField(widget = forms.RadioSelect(), choices=PAGE_THREE_CHOICE_SET)
+    exercise_not = forms.ChoiceField(widget = forms.RadioSelect(), choices=PAGE_THREE_CHOICE_SET)
+    
+    def __init__(self, *args, **kwargs):
+        super(PageThree, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.add_input(Submit('submit', 'Next Page'))
+        self.helper.layout = Layout('',
+                                    InlineRadios('exercise_stay_health'),
+                                    InlineRadios('exercise_lose_weight'),
+                                    InlineRadios('exercise_improve_appearance'),
+                                    InlineRadios('exercise_energize'),
+                                    InlineRadios('exercise_sport'),
+                                    InlineRadios('exercise_fun'),
+                                    InlineRadios('exercise_muscle'),
+                                    InlineRadios('exercise_not'),)
+
+    class Meta:
+        model = Survey
+        fields = ('exercise_stay_health', 'exercise_lose_weight', 'exercise_improve_appearance', 'exercise_energize',
+                    'exercise_sport', 'exercise_fun', 'exercise_muscle', 'exercise_not',)
+
+class PageFour(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PageFour, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.add_input(Submit('submit', 'Next Page'))
+
+    class Meta:
+        model = Survey
+        fields = ('strenuous_activity_type', 'moderate_activity_type', 'strength_activity_type',
+                    'strenuous_activity_days', 'moderate_activity_days', 'strength_activity_days',)
+
+class PageFive(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PageFive, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.add_input(Submit('submit', 'Next Page'))
+
+    class Meta:
+        model = Survey
+        fields = ('self_esteem', 'time_to_exercise', 'exercise_enjoyable', 'exercise_risk_age', 'exercise_poor_health', 'exercise_lack_skills',
+                    'exercise_lack_funds', 'exercise_intimidation', 'exercise_get_enough', 'low_self_esteem', 'exercise_lack_access',
+                    'exercise_safety_concern', 'exercise_inconvenient', 'exercise_goals', 'exercise_lgbtq_friendly', 'exercise_space_friendly',
+                    'improve_lgbtq_spaces',)
+
---
+++ survey/migrations/0002_auto__chg_field_survey_gender_assigned.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Survey.gender_assigned'
+        db.alter_column(u'survey_survey', 'gender_assigned', self.gf('django.db.models.fields.CharField')(max_length=20))
+
+    def backwards(self, orm):
+
+        # Changing field 'Survey.gender_assigned'
+        db.alter_column(u'survey_survey', 'gender_assigned', self.gf('django.db.models.fields.CharField')(max_length=2))
+
+    models = {
+        u'survey.survey': {
+            'Meta': {'object_name': 'Survey'},
+            'age': ('django.db.models.fields.IntegerField', [], {'max_length': '3'}),
+            'current_body_shape_hips': ('django.db.models.fields.IntegerField', [], {'max_length': '3'}),
+            'current_body_shape_shoulders': ('django.db.models.fields.IntegerField', [], {'max_length': '3'}),
+            'current_body_shape_waist': ('django.db.models.fields.IntegerField', [], {'max_length': '3'}),
+            'ethnicity': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'ethnicity_other': ('django.db.models.fields.CharField', [], {'max_length': '40', 'null': 'True', 'blank': 'True'}),
+            'excercise_lose_weight': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_encouragement': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_energize': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_enjoyable': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_fun': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_get_enough': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_goals': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_improve_appearance': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_inconvenient': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_intimidation': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_lack_access': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_lack_funds': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_lack_skills': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_lgbtq_friendly': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
+            'exercise_muscle': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_not': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_poor_health': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_risk_age': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_safety_concern': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_space_friendly': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'exercise_sport': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'exercise_stay_health': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'gender_assigned': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'gender_identity': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'gender_identity_other': ('django.db.models.fields.CharField', [], {'max_length': '40', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ideal_body_shape_hips': ('django.db.models.fields.IntegerField', [], {'max_length': '3'}),
+            'ideal_body_shape_shoulders': ('django.db.models.fields.IntegerField', [], {'max_length': '3'}),
+            'ideal_body_shape_waist': ('django.db.models.fields.IntegerField', [], {'max_length': '3'}),
+            'improve_lgbtq_spaces': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'low_self_esteem': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'moderate_activity_days': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'moderate_activity_type': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'other_reasons': ('django.db.models.fields.TextField', [], {}),
+            'self_esteem': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'sexual_orientation': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'sexual_orientation_other': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
+            'state': ('django_localflavor_us.models.USStateField', [], {'max_length': '2'}),
+            'strength_activity_days': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'strength_activity_type': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'strenuous_activity_days': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'strenuous_activity_type': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'time_to_exercise': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'zipcode': ('django.db.models.fields.CharField', [], {'max_length': '10'})
+        }
+    }
+
+    complete_apps = ['survey']
