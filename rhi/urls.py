9a6c847771e3884570b1be4f732d12794d724c8d
Kevin Klaes <kevin.klaes@gmail.com>
2013-07-28 19:44:38 -0500

    Added admin functionality.

--- rhi/urls.py
+++ rhi/urls.py
@@ -4,8 +4,8 @@ from survey.views import *
 from survey.forms import *
 
 # Uncomment the next two lines to enable the admin:
-#from django.contrib import admin
-#admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url('^$', SurveyWizard.as_view([PageOne, PageTwo, PageThree, PageFour, PageFive]), name='rhi-form'),
@@ -17,5 +17,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
